30bbd98a66be8da50d1e4681bbbb3f0f79a30fe7
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-08 11:53:18 +0530

    update

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -2,5 +2,5 @@ module "aws_vpc" {
   source        = "../../terraform-central/vpc"
   project       = var.project
   environment   = var.environment
-  is_peering_required = true
+  is_peering_required = false
 }
