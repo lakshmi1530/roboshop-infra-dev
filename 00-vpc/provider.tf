f24722fe4a67b7afc53a167e911f6754cac82bdb
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-10 17:58:55 +0530

    update

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -9,7 +9,7 @@ terraform {
   #configuring remote state in AWS S3
   backend "s3" {
     bucket         = "remote-state-aws-pratyu-dev" # repalce with unique bucket name
-    key            = "roboshop-dev-vpc" #key should be changed elase the same .tfstate will updtae which is wrong
+    key            = "roboshop-dev-vpc.tfstate" #key should be changed else the same .tfstate will updtae which is wrong
     region         = "us-east-1"
     encrypt = true
     use_lockfile = true  #this lock is mandatory to make sure .state file is not corupted when different users are using
