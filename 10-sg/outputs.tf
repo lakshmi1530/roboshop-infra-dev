ff23e85bfd37d6b2cdf6b99a98bb98d5eb47c9b4
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-10 18:41:00 +0530

    update

--- 10-sg/outputs.tf
+++ 10-sg/outputs.tf
@@ -1,3 +1,3 @@
 output "sg_id" {
-  value = module.aws_security_group.id
+  value = module.aws_security_group.sg_id
 }
--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "sg_id" {
-  name = "/${var.project}/${var.environment}/sg_id"
+resource "aws_ssm_parameter" "mongo_sg_id" {
+  name = "/${var.project}/${var.environment}/mongo_sg_id"
   type = "String"
   value = module.aws_security_group.sg_id
 }
