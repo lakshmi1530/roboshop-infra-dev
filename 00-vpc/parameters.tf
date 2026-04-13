2c404604e8a66f8c20b5d1d0cd26d104e4d7d5b8
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-13 12:28:50 +0530

    update

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -7,17 +7,21 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project}/${var.environment}/public_subnet_ids"
   type = "StringList"
-  value = convertlist(string, module.aws_vpc.public_subnet_ids)
+  value = join(",", module.aws_vpc.public_subnet_ids) 
 }
+# converting list of string to string list as SSM parameter type is StringList and value should be comma seperated string
+# example: if private_subnet_ids = ["subnet-12345", "subnet-67890"]
+# then join(",", module.aws_vpc.private_subnet_ids) will convert it to "subnet-12345,subnet-67890"
+# which is the expected format for StringList type in AWS SSM parameter store
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
   type = "StringList"
-  value = convertlist(string, module.aws_vpc.private_subnet_ids) # converting list of string to string list as SSM parameter type is StringList and value should be comma seperated string
+  value = join(",", module.aws_vpc.private_subnet_ids) 
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project}/${var.environment}/database_subnet_ids"
   type = "StringList"
-  value = convertlist(string, module.aws_vpc.database_subnet_ids)
+  value = join(",", module.aws_vpc.database_subnet_ids) 
 }
