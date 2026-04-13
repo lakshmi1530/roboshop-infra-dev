0b9a74ae54d7aac1c4e42338c7c81d58d18dce62
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-13 12:08:59 +0530

    update

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -1,3 +1,14 @@
 output "vpc_id" {
   value = module.aws_vpc.vpc_id
 }
+ output "public_subnet_ids" {
+  value = module.aws_vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.aws_vpc.private_subnet_ids
+}
+
+output "db_subnet_group_name" {
+  value = module.aws_vpc.db_subnet_group_name
+}
