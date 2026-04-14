1776727e914c677481a3e1edcd9b9878b80e974f
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-14 15:32:18 +0530

    update

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -4,13 +4,21 @@ resource "aws_instance" "bastion" {
   subnet_id     = local.public_subnet_ids
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+   # Using user_data to install Terraform on the instance itself
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y yum-utils
+              sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+              sudo yum -y install terraform
+              EOF
+
   tags = merge(
     local.common_tags, 
-  {
-    Name = "${var.project}-${var.environment}-bastion"
-  }
+    {
+      Name = "${var.project}-${var.environment}-bastion"
+    }
   )
-
 }
 
 resource "aws_iam_role" "bastion_role" {
