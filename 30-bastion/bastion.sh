bdd6fee432543acc75d90a4d39a63dfafc95606c
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-14 16:54:46 +0530

    update

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -2,10 +2,20 @@
 #we are creating 50GB root disk, but inly 20GB is partitioned
 #Remining 30GB is unpartitioned, we need to extend using below command
 
-growpart /dev/nvme0n1 4
-lvextend -r -L +30GB /dev/mapper/RootVG-homeVol
-xfs_growfs /home
 
+# Install required tools
+sudo yum install -y cloud-utils-growpart
+
+# Extend partition
+sudo growpart /dev/nvme0n1 4
+
+# Extend LVM using ALL free space (recommended)
+sudo lvextend -l +100%FREE /dev/mapper/RootVG-homeVol
+
+# Resize filesystem
+sudo xfs_growfs /home
+
+# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+sudo yum install -y terraform
