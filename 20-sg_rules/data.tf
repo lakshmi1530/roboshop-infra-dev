22a4b2b38e0997bd6beb90c29b1cd57ed88cf523
lakshmi1530 <lakshmi.p.mukkamalla@gmail.com>
2026-04-15 15:26:24 +0530

    update

--- 20-sg_rules/data.tf
+++ 20-sg_rules/data.tf
@@ -21,3 +21,35 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "redis_sg_ids" {
   name = "/${var.project}/${var.environment}/redis-sg-id"
 }
+
+data "aws_ssm_parameter" "rabbitmq_sg_ids" {
+  name = "/${var.project}/${var.environment}/rabbitmq-sg-id"
+}
+
+data "aws_ssm_parameter" "mysql_sg_ids" {
+  name = "/${var.project}/${var.environment}/mysql-sg-id"
+}
+
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project}/${var.environment}/shipping-sg-id"
+}
+
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project}/${var.environment}/cart-sg-id"
+}
+
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project}/${var.environment}/payment-sg-id"
+}
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/backend-alb-sg-id"
+}
+
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/frontend-alb-sg-id"
+}
+
+data "aws_ssm_parameter" "frontend_sg_id" {
+  name = "/${var.project}/${var.environment}/frontend-sg-id"
+}
